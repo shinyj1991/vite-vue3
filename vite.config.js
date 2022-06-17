6e66bc40b2801e8d276d56223427bffb37a040b6
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-06-17 13:14:32 +0900

    env, action

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '@scss': resolve(__dirname, 'src/assets/scss'),
-      '@images': resolve(__dirname, 'src/assets/images'),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        '@scss': resolve(__dirname, 'src/assets/scss'),
+        '@images': resolve(__dirname, 'src/assets/images'),
+      },
     },
-  },
-  server: {
-    open: false,
-    port: 3000,
-  },
+    server: {
+      open: false,
+      port: 3000,
+    },
+    base: {
+      base: env.VITE_ASSET_BASE
+    }
+  }
 })
